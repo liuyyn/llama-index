3883134da295aeda84116e84bd5f51f0bed6ccb5
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2025-02-08 00:18:49 -0500

    get attractions data from wikipedia and tripadvisor

---
+++ attractions_recommendation/get_data.py
@@ -0,0 +1,33 @@
+import requests
+import os
+from dotenv import load_dotenv
+import wikipediaapi
+
+load_dotenv()
+TRIPADVISOR_API_KEY = os.environ["TRIPADVISOR_API_KEY"]
+
+
+def get_data_from_tripadvisor(city: str, country: str):
+    url = f"https://api.content.tripadvisor.com/api/v1/location/search?key={TRIPADVISOR_API_KEY}&searchQuery={city}%2C%20{country}&category=attractions&language=en"
+
+    headers = {"accept": "application/json"}
+
+    response = requests.get(url, headers=headers)
+
+    return response.json().get("data")
+
+
+def get_data_from_wikipedia(city: str):
+    wiki_wiki = wikipediaapi.Wikipedia(
+        user_agent="MyProjectName (merlin@example.com)",
+        language="en",
+        extract_format=wikipediaapi.ExtractFormat.WIKI,
+    )
+
+    wiki_page = wiki_wiki.page(city)
+
+    return wiki_page.text
+
+
+def get_data():
+    pass
---
+++ attractions_recommendation/readme.md
@@ -0,0 +1,5 @@
+### Attractions recommendation system 
+
+Given a city, get data about this city's attractions. 
+Index the data and build the RAG. 
+Plug the RAG to the LLM to get grounded attractions recommendations for the given city.
