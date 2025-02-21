f815b0c3f3b54f363832fae9f7f420496d51ef8b
Yu Yun Liu <yu.yun.liu@mail.mcgill.ca>
2025-02-21 17:02:02 -0500

    cleanup

--- attractions_recommendation/index.py
+++
@@ -1,5 +0,0 @@
-from llama_index.core import VectorStoreIndex
-
-# to build an index, you need to first load the data and create a vector store
-# from the vector store, you can create an index
-# you can reload and update the index by updating the vector store with new data and rebuilding the index
